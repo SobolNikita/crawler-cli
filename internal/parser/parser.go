160e2bb0f80a20a70111032f546599ee423b6738
Sobol Nikita <soboln2006@gmail.com>
2026-09-24 23:41:49 +0300

    feat: implement parser.go

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,56 @@
 package parser
+
+import (
+	"bytes"
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type ParseResult struct {
+	Title string
+	Links []string
+}
+
+func ParseHTML(body []byte) (ParseResult, error) {
+	var result ParseResult
+
+	z := html.NewTokenizer(bytes.NewReader(body))
+
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			if z.Err() == io.EOF {
+				return result, nil
+			}
+			return result, z.Err()
+
+		case html.StartTagToken, html.SelfClosingTagToken:
+			name, hasAttr := z.TagName()
+
+			if string(name) == "title" {
+				if z.Next() == html.TextToken {
+					result.Title = strings.TrimSpace(string(z.Text()))
+				}
+				continue
+			}
+
+			if string(name) == "a" && hasAttr {
+				for {
+					key, val, more := z.TagAttr()
+					if string(key) == "href" {
+						href := strings.TrimSpace(string(val))
+						if href != "" {
+							result.Links = append(result.Links, href)
+						}
+					}
+					if !more {
+						break
+					}
+				}
+			}
+		}
+	}
+}
