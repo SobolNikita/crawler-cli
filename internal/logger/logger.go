205c948b1538679ed82a7d2bee784fbe92a671be
Sobol Nikita <soboln2006@gmail.com>
2026-09-25 00:02:26 +0300

    feat: implement logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,44 @@
 package logger
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+type Logger struct {
+	file *os.File
+	mu   sync.Mutex
+}
+
+func New(path string) (*Logger, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Logger{file: f}, nil
+}
+
+func (l *Logger) Close() error {
+	if l == nil || l.file == nil {
+		return nil
+	}
+	return l.file.Close()
+}
+
+func (l *Logger) Status(url string, statusCode int) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	fmt.Fprintf(l.file, "%s STATUS %s %d\n", time.Now().Format(time.RFC3339), url, statusCode)
+}
+
+func (l *Logger) Error(url string, err error) {
+	if err == nil {
+		return
+	}
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	fmt.Fprintf(l.file, "%s ERROR %s %v\n", time.Now().Format(time.RFC3339), url, err)
+}
